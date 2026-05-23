5edf0bbe07e6b59db5e3d8bfff9b474c8c7b8e09
Rodion <rodiongork@github.com>
2026-05-23 10:34:46 +0300

    fixed crypter

--- crypter.pl
+++ crypter.pl
@@ -1,6 +1,7 @@
 use v5.16;
 use warnings;
 use Digest::SHA;
+use Digest::MD5;
 use MIME::Base64;
 
 die 'need pwd as firs cmd line argument' unless @ARGV > 0;
@@ -19,17 +20,19 @@ while (my $line = <STDIN>) {
             $decode = 0;
         }
     }
-    if (!$decode) {
+    if ($line eq '') {
+        say '';
+    } elsif (!$decode) {
         my @bytes = split //, $line;
-        my $salt = int(rand(899) + 100);
+        my $salt = substr Digest::MD5::md5_base64($line), -4;
         my @key = split //, Digest::SHA::sha512($salt . $pwd);
         for (my $i = 0; $i < @bytes; $i++) {
             $bytes[$i] ^= $key[$i%@key];
         }
         say $salt . MIME::Base64::encode_base64(join('', @bytes), '');
     } else {
-        my $salt = substr $line, 0, 3;
-        my @bytes = split //, MIME::Base64::decode_base64(substr $line, 3);
+        my $salt = substr $line, 0, 4;
+        my @bytes = split //, MIME::Base64::decode_base64(substr $line, 4);
         my @key = split //, Digest::SHA::sha512($salt . $pwd);
         for (my $i = 0; $i < @bytes; $i++) {
             $bytes[$i] ^= $key[$i%@key];
